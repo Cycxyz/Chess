ccb7fec026a4f862b9da0b1ac78336be24d7c3e4
blindblackholee <gerasimenko.aleksey.a@gmail.com>
2022-07-07 01:44:27 +0300

    fix: changed encoding

--- head/Figure.h
+++ head/Figure.h
@@ -8,51 +8,51 @@
 
 enum class FigureType
 {
-	Figure,
-	Bishop,
-	Rook,
-	King,
-	Queen,
-	Pawn,
-	Horce
+    Figure,
+    Bishop,
+    Rook,
+    King,
+    Queen,
+    Pawn,
+    Horce
 };
-//Координаты вида (0,0)
+//пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅ (0,0)
 struct Cells
 {
-	int x, y;
+    int x, y;
 };
 bool operator==(const Cells& c1, const Cells& c2);
 bool operator<(Cells c1, Cells c2);
 class Figure
 {
 public:
-	Figure(FigureType type_)
-	{
-		type = type_;
-		cell. x = -1;
-		cell.y = -1;
-		MadeMove = 0;
-		CastlingL = Cells{ -1,-1 };
-		CastlingR = Cells{ -1,-1 };
-		direction = 0;
-	}
-	std::set<Cells> allowed;
-	Cells cell;
-	bool MadeMove=0;
-	bool isWhite;
-	int direction;
-	Cells CastlingR;
-	Cells CastlingL;
-	sf::Image image;
-	sf::Texture texture;
-	sf::Sprite sprite;
-	FigureType type;
-	virtual void CalculateAllowed(std::vector<std::shared_ptr<Figure>>& figures);
-	virtual void CalculateAllowedForbidden(std::vector<std::shared_ptr<Figure>>& figures);
-	 virtual void SetSprite(bool isWhite) = 0;
-	  void SetPosition(int x_, int y_);
+    Figure(FigureType type_)
+    {
+        type = type_;
+        cell. x = -1;
+        cell.y = -1;
+        MadeMove = 0;
+        CastlingL = Cells{ -1,-1 };
+        CastlingR = Cells{ -1,-1 };
+        direction = 0;
+    }
+    std::set<Cells> allowed;
+    Cells cell;
+    bool MadeMove=0;
+    bool isWhite;
+    int direction;
+    Cells CastlingR;
+    Cells CastlingL;
+    sf::Image image;
+    sf::Texture texture;
+    sf::Sprite sprite;
+    FigureType type;
+    virtual void CalculateAllowed(std::vector<std::shared_ptr<Figure>>& figures);
+    virtual void CalculateAllowedForbidden(std::vector<std::shared_ptr<Figure>>& figures);
+     virtual void SetSprite(bool isWhite) = 0;
+      void SetPosition(int x_, int y_);
 private:
-	void SeparateForbidden();
+    void SeparateForbidden();
 };
 #define Figures  std::vector<std::shared_ptr<Figure>>&
 int FindFigure(int x, int y, Figures figures);
--- src/Chesss.cpp
+++ src/Chesss.cpp
@@ -1,4 +1,4 @@
-﻿#include <SFML/Graphics.hpp>
+#include <SFML/Graphics.hpp>
 #include "Pawn.h"
 #include"King.h"
 #include"Queen.h"
